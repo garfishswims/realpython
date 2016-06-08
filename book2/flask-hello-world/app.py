d66aa15af2424efc13b37cc05d66b51f48c426ab
Nick Jacob <nick@thehumangeo.com>
2016-06-07 21:40:29 -0400

    add converters to query parameters

--- book2/flask-hello-world/app.py
+++ book2/flask-hello-world/app.py
@@ -19,6 +19,22 @@ def hello_world():
 def search(search_query):
     return search_query
 
+@app.route('/integer/<int:value>')
+def int_type(value):
+    print(value + 1)
+    return 'correct'
+
+@app.route('/float/<float:value>')
+def float_type(value):
+    print(value + 1)
+    return 'correct'
+
+# dynamic route that accepts slashes
+@app.route('/path/<path:value>')
+def path_type(value):
+    print(value)
+    return 'correct'
+
 # start the dev server using the run() method
 if __name__ == '__main__':
     app.run()
