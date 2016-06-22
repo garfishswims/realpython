8efa8882dfa8feabbf704074e2e4e29b0fcefc15
Nick Jacob <nick@thehumangeo.com>
2016-06-22 16:52:26 -0400

    add login_required decorator to controller

--- book2/flask-blog/blog.py
+++ book2/flask-blog/blog.py
@@ -9,6 +9,7 @@ from flask import flash
 from flask import redirect
 from flask import url_for
 from flask import g
+from functools import wraps
 import sqlite3
 
 # configuration
@@ -26,6 +27,16 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
@@ -38,15 +49,16 @@ def login():
             return redirect(url_for('main'))
     return render_template('login.html', error=error)
 
+@app.route('/main')
+@login_required
+def main():
+    return render_template('main.html')
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('login'))
-
-@app.route('/main')
-def main():
-    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
